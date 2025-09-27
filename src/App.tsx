92cebbab4faed9b8861c2321e87f11bbf2cb779e
Ryan <131712164+ryanNS3@users.noreply.github.com>
2025-09-26 22:28:07 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css'
+import { PlusRegular, PlusRegularRose } from './assets/Plus-Regular'
+import { Button } from './Components/Button/Button'
 import { TrashPrimary, TrashSecondary, TrashTerciary } from './Components/Button/Trash'
+import { Check } from './Components/Form/Check'
 
 function App() {
 
@@ -9,6 +12,8 @@ function App() {
       <TrashPrimary/>
       <TrashSecondary/>
       <TrashTerciary/>
+      <Button text='Tes' icon={<PlusRegularRose/>}/>
+      <Check/>
     </>
   )
 }
