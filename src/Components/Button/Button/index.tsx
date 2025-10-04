ca44bb96250e77f1c56f07c0a14dbb15062646fa
ryansantosn333 <ryansantosn333@gmail.com>
2025-10-04 00:59:14 +0000

    fix/ajustando os componentes

--- src/Components/Button/Button/index.tsx
+++ src/Components/Button/Button/index.tsx
@@ -1,14 +1,15 @@
 import type { ReactNode } from "react"
 
 interface ButtonInterface{
-    text: string,
-    icon: ReactNode
+    text: string;
+    icon: ReactNode;
+    onClick: () => void;
 }
 
 
-export function Button({text, icon}: ButtonInterface){
+export function Button({text, icon, onClick}: ButtonInterface){
     return(
-        <button className="flex items-center justify-center gap-2 text-body-md font-semibold  min-w-[31.5rem] px-5 py-4 rounded-lg  bg-gray-200 hover:bg-pink-light "> {icon} {text}</button>
+        <button onClick={onClick} className="flex items-center justify-center gap-2 text-body-md font-semibold  md:min-w-[31.5rem] px-5 py-4 rounded-lg  bg-gray-200 hover:bg-pink-light "> {icon} {text}</button>
     )
 }
 
